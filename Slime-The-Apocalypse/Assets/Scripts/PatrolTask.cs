6b7b65811c6cedbe106482ea56c49aadf87bc1d5
Brendan Halstead <Brendan_Halstead@live.com>
2022-11-07 17:50:38 -0500

    animation and player movement quick fix

--- Slime-The-Apocalypse/Assets/Scripts/PatrolTask.cs
+++ Slime-The-Apocalypse/Assets/Scripts/PatrolTask.cs
@@ -24,6 +24,7 @@ public class PatrolTask : BTNode
         {
             Debug.Log("Patrol Not Spotted");
             rb.velocity = new Vector2(/*speed*/ 500f * Time.deltaTime, rb.velocity.y);
+            collider.IsTouchingLayers(/*Obstacles*/);
         }
         else
         {
--- Slime-The-Apocalypse/Assets/Scripts/PlayerMovment.cs
+++ Slime-The-Apocalypse/Assets/Scripts/PlayerMovment.cs
@@ -9,7 +9,7 @@ public class PlayerMovment : MonoBehaviour
     private float direction;
 
     //Animtorcontroller of the player.
-    public Animator player_move_action;
+    public Animator playerMoveAction;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +19,18 @@ public class PlayerMovment : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Horizontal") != 0)
+            playerMoveAction.SetBool("isWalk", true);
+        else
+            playerMoveAction.SetBool("isWalk", false);
+
         direction = Input.GetAxis("Horizontal");
         rb.velocity = new Vector2(speed * direction, rb.velocity.y);
         if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
         {
             rb.AddForce(new Vector2(rb.velocity.x, 500f));
-            player_move_action.SetBool("Jump", true);
-            player_move_action.SetBool("OnGround", false);
+            playerMoveAction.SetBool("Jump", true);
+            playerMoveAction.SetBool("OnGround", false);
         }
     }
 
@@ -34,8 +39,8 @@ public class PlayerMovment : MonoBehaviour
         Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag.Equals("Ground"))
         {
-            player_move_action.SetBool("OnGround", true);
-            player_move_action.SetBool("Jump", false);
+            playerMoveAction.SetBool("OnGround", true);
+            playerMoveAction.SetBool("Jump", false);
         }
     }
 
