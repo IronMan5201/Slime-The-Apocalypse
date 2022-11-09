83618636483cac90798c2e4f317ef6d681f97808
IronMan5201 <jcourcha@gmu.edu>
2022-11-09 02:03:20 -0500

    Initial UI for level 1 complete. Pause Menu and Time for level also implemented. All buttons should work

---
+++ Slime-The-Apocalypse/Assets/Scripts/LevelTime.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class LevelTime : MonoBehaviour
+{
+
+    public float currentTime;
+
+    public int currentTimeGUI;
+
+    public TextMeshProUGUI timeGUI;
+
+    public PauseMenu menu;
+
+    public int DelayAmount = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0.0f;
+        currentTimeGUI = 0;
+        timeGUI.text = "Time-" + currentTimeGUI.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!menu.IsPaused())
+        {
+            currentTime += Time.deltaTime;
+            timeGUI.text = "Time-" + System.TimeSpan.FromSeconds(currentTime).ToString("mm':'ss");
+            /*if(currentTime >= DelayAmount)
+            {
+                currentTime = 0f;
+                currentTimeGUI++;
+                timeGUI.text = "Time-" + System.TimeSpan.FromSeconds(currentTime).ToString("mm':'ss");
+            }*/
+        }
+    }
+}
---
+++ Slime-The-Apocalypse/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    public GameObject backgroundMusic;
+
+    public GameObject infoDisplay;
+
+
+    //Resume the game from the pause menu
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        infoDisplay.SetActive(true);
+        Time.timeScale = 1f;  //set the scale at which time passes to 1, normal rate
+        GameIsPaused = false;
+        backgroundMusic.GetComponent<AudioSource>().volume += 0.9f;
+    }
+
+    //pause the game when the pause button is clicked
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        infoDisplay.SetActive(false);
+        Time.timeScale = 0f;  //set the scale at which time passes to 0, frozen, paused
+        GameIsPaused = true;
+        backgroundMusic.GetComponent<AudioSource>().volume -= 0.9f;
+    }
+
+    public void LoadMenu(string sceneName)
+    {
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        SceneManager.LoadScene(sceneName);  //loads scene from build settings at index 0, which is our main menu
+    }
+
+    public bool IsPaused()
+    {
+        return GameIsPaused;
+    }
+}
