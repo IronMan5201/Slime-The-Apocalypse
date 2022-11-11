75b442ae28b9de4290d63b8ef55247b0bca7045d
Brendan Halstead <Brendan_Halstead@live.com>
2022-11-10 21:40:14 -0500

    ablitiy UI quick change and build settings

--- Slime-The-Apocalypse/Assets/Scripts/AbilityUI.cs
+++ Slime-The-Apocalypse/Assets/Scripts/AbilityUI.cs
@@ -21,6 +21,25 @@ public class AbilityUI : MonoBehaviour
     void Update()
     {
         powerUp = player.GetComponent<PlayerMovment>().currentPowerUp;
-        abilityGUI.text = "Ability:\n" + powerUp.ToString();
+        string text = "NONE";
+        switch (powerUp)
+        {
+            case PowerUp.NONE:
+                text = "NONE";
+                break;
+            case PowerUp.JUMP:
+                text = "DOUBLE JUMP";
+                break;
+            case PowerUp.SHOT:
+                text = "SWALLOW SHOT";
+                break;
+            case PowerUp.HOVER:
+                text = "HOVER";
+                break;
+            default:
+                break;
+        }
+
+        abilityGUI.text = "Ability:\n" + text;
     }
 }
