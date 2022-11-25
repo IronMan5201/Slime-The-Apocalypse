32d9b28f7d8b5bc6a687d0ecc5acba28f5adaffe
Brendan Halstead <Brendan_Halstead@live.com>
2022-11-25 18:03:15 -0500

    rat enemy and AI tweaking

--- Slime-The-Apocalypse/Assets/Scripts/MeleeBT.cs
+++ Slime-The-Apocalypse/Assets/Scripts/MeleeBT.cs
@@ -7,10 +7,26 @@ public class MeleeBT : BehaviorTree.Tree
 {
     public static float speed = 3f;
     public LayerMask mask;
+    public LayerMask blocker;
+    public LayerMask button;
     protected override BTNode SetUpTree()
     {
-        BTNode root = new PatrolTask(gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Rigidbody2D>(), gameObject.GetComponent<Transform>(), mask);
+        Collider2D temp2DC = null;
+        Rigidbody2D rig2D = null;
+
+        if (gameObject.GetComponent<Collider2D>() != null)
+            temp2DC = gameObject.GetComponent<Collider2D>();
+        else
+            temp2DC = transform.GetChild(0).GetComponent<Collider2D>();
+
+        if (gameObject.GetComponent<Rigidbody2D>() != null)
+            rig2D = gameObject.GetComponent<Rigidbody2D>();
+        else
+            rig2D = transform.GetChild(0).GetComponent<Rigidbody2D>();
+
+        BTNode root = new PatrolTask(temp2DC, rig2D, gameObject.GetComponent<Transform>(), mask, button, blocker);
 
         return root;
+
     }
 }
--- Slime-The-Apocalypse/Assets/Scripts/PatrolTask.cs
+++ Slime-The-Apocalypse/Assets/Scripts/PatrolTask.cs
@@ -10,11 +10,15 @@ public class PatrolTask : BTNode
     private Transform transform;
     private Collider2D platformTracker;
     public LayerMask mask;
+    public LayerMask blocker;
+    public LayerMask button;
     private bool flip = false;
+    private bool jumped = false;
 
+    private Vector2 lastPos;
 
     // Start is called before the first frame update
-    public PatrolTask(Collider2D collider, Rigidbody2D rigidbody, Transform transform, LayerMask mask)
+    public PatrolTask(Collider2D collider, Rigidbody2D rigidbody, Transform transform, LayerMask mask, LayerMask button, LayerMask blocker)
     {
         spottedPlayer = false;
         rb = rigidbody;
@@ -22,33 +26,51 @@ public class PatrolTask : BTNode
         this.transform = transform;
         platformTracker = transform.Find("Tracker").GetComponent<Collider2D>();
         this.mask = mask;
+        this.button = button;
+        this.blocker = blocker;
     }
 
     public override NodeState Evaluate()
     {
-        if (!spottedPlayer)
+        if (Time.time >= 1f)
         {
-            Debug.Log("Patrol Not Spotted");
-            rb.velocity = new Vector2( (flip ? -1 : 1) * /*speed*/ 500f * Time.deltaTime, rb.velocity.y);
-            Debug.Log("TEST");
-            Debug.Log("plat: " + platformTracker.IsTouchingLayers(mask));
-
-            if (!platformTracker.IsTouchingLayers(mask) && collider.IsTouchingLayers(mask))
+            if (!spottedPlayer)
             {
-                flip = !flip ;
-                transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
-                Debug.Log("Hit");
+                Debug.Log("Patrol Not Spotted");
+                rb.velocity = new Vector2((flip ? -1 : 1) * /*speed*/ 500f * Time.deltaTime, rb.velocity.y);
+                Debug.Log("TEST");
+                Debug.Log("plat: " + platformTracker.IsTouchingLayers(mask));
+
+                if (collider.IsTouchingLayers(button))
+                    rb.velocity = new Vector2(rb.velocity.x, 250f * Time.deltaTime);
+
+                if ((!platformTracker.IsTouchingLayers(mask) && collider.IsTouchingLayers(mask)) || platformTracker.IsTouchingLayers(blocker))
+                {
+                    flip = !flip;
+                    transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+                    Debug.Log("Hit");
+                }
+            }
+            else
+            {
+                Debug.Log("Patrol Spotted");
+                rb.velocity = new Vector2(/*speed*/ 5f * Time.deltaTime, rb.velocity.y);
             }
         }
-        else
-        {
-            Debug.Log("Patrol Spotted");
-            rb.velocity = new Vector2(/*speed*/ 5f * Time.deltaTime, rb.velocity.y);
-        }
-
         state = NodeState.RUNNING;
         return state;
+        
+    }
+    IEnumerator Jump()
+    {
+        jumped = true;
+        rb.velocity = new Vector2(rb.velocity.x, 250f * Time.deltaTime);
+        yield return new WaitForSeconds(0.5f);
+        jumped = false;
     }
 
-    
+
 }
+
+
+
